5096b07aaf9883ecbdd1644ef5617d7a862994f5
sam tadros <samer1787rizk@gmail.com>
2024-07-16 20:45:26 +0300

    a better shit cuz , it's still shit

--- tcp_test/client/src/main.rs
+++ tcp_test/client/src/main.rs
@@ -2,17 +2,15 @@
 //#![warn(unused_imports)]
 use std::io;
 use std::io::prelude::*;
-//use std::io::{Read, Write};
 //use std::net::TcpListener;
 use std::net::TcpStream;
-
-const STATE: u8 = 1;
 
 fn main() -> io::Result<()> {
     let mut server = TcpStream::connect("0.0.0.0:1909").expect("fail to connect");
     println!("connected to port");
     let msg = server.read(&mut [1])?;
-    println!("server read");
-    //server.write(&mut [2])?;
+    println!("server read {}", msg);
+    let _ = server.write(&mut [2])?;
+    let _ = server.flush();
     Ok(())
 }
--- tcp_test/server/src/main.rs
+++ tcp_test/server/src/main.rs
@@ -1,8 +1,9 @@
 #![warn(unused_variables)]
 //#![warn(unused_imports)]
-
 use std::io;
-use std::io::Write;
+use std::io::prelude::*;
+//use tokio::net::TcpListener;
+//use tokio::net::TcpStream;
 use std::net::TcpListener;
 use std::net::TcpStream;
 
@@ -10,12 +11,11 @@ const STATE: u8 = 1;
 
 fn handle_connection(mut stream: TcpStream) -> io::Result<()> {
     //let state: u8 = 0;
-    //let buff = 1;
 
-    stream.write(&STATE.to_be_bytes());
+    let _ = stream.write(&STATE.to_be_bytes());
     let _ = stream.flush();
-    //let msg = stream.read(&mut [0; 3])?;
-    //println!("client: {}", &msg);
+    let msg = stream.read(&mut [2])?;
+    println!("client: {}", &msg);
     println!("passed to handle_connection");
     Ok(())
 }
