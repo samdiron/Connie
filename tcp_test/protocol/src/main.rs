bf6867c98b3020861f3b2c87ebac1b933f1b961f
sam tadros <samer1787rizk@gmail.com>
2024-07-23 17:22:29 +0300

    delete shit code start all will be replaced by code i did in the past few days

--- tcp_test/protocol/src/main.rs
+++ tcp_test/protocol/src/main.rs
@@ -1,21 +1,3 @@
-use surrealdb::Surreal;
-use surrealdb::sql::Thing;
-use surrealdb::opt::auth::Root;
-use surrealdb::engine::remote::ws::Ws;
-use tokio;
-use bcrypt::{hash, DEFAULT_COST};
-use srade_json::json;
-
-
-#[tokio::main]
-async fn main() -> Result<(),Box<dny std::error::Error>> {
-    //connect to db
-    let db = Surreal::new::<Ws>(0.0.0.0:8000).await?;
-
-    //sgin in
-    db.sginin(Root{
-        username: "root",
-        password: "root",
-    }).await?;
-    db.
+fn main() {
+    println!("gaga");
 }
