8e4f34fc0def6b02eb43a7600750b32da08cad85
samdiron <samer1787rizk@gmail.com>
2024-09-06 18:28:10 +0300

    not working but i'm crying rn will do it after

--- tcp_test/server/src/main.rs
+++ tcp_test/server/src/main.rs
@@ -1,26 +1,36 @@
-use std::io::{ErrorKind, Write};
+use std::io::{Error, ErrorKind, Write};
 use std::sync::Arc;
 use rustls::pki_types::{CertificateDer, PrivateKeyDer };
 use rustls::server::{Acceptor, NoClientAuth};
-use rustls::{ServerConfig};
+use rustls::{ConfigBuilder, ServerConfig};
 use std::io::{self, BufReader};
 use std::fs::File;
-use rcgen::Certificate;
+use rustls::crypto::CryptoProvider;
+//use rcgen::Certificate;
 use rustls_pemfile;
 
-const CERT : io::Result<Vec<CertificateDer>> = load_cert("~/.config/connie/certificates/cert.pem");
-const PRIVATE_KEY : io::Result<PrivateKeyDer> = load_private_certificate_key("~/.config/connie/keys/key.pem");
-
-
+//static CERT : Vec<CertificateDer> = load_cert("~/.config/connie/certificates/cert.pem");
+//static PRIVATE_KEY : Vec<PrivateKeyDer> = load_private_certificate_key("~/.config/connie/keys/key.pem");
+// const CERT : io::Result<Vec<CertificateDer>> = load_cert("~/.config/connie/certificates/cert.pem");
+// const PRIVATE_KEY : io::Result<PrivateKeyDer> = load_private_certificate_key("~/.config/connie/keys/key.pem");
+//
 fn main() {
-        env_logger::init();
+        //env_logger::init();
         //let pki = TestPki::new();
         //let server_config = pki.server_config();
-        let mut server_config = ServerConfig::new(NoClientAuth::new());
-        server_config.set_single_cert(CERT, PRIVATE_KEY);
+        let cert: Vec<CertificateDer> = load_cert("~/.config/connie/certificates/cert.pem").unwrap();
+        let private_key: Vec<PrivateKeyDer> = load_private_certificate_key("~/.config/connie/keys/key.pem").unwrap();
+        let private_key :PrivateKeyDer = private_key.iter().next().unwrap().clone_key();
+        println!("private key is not a vec ");
 
+        let mut server_config   = ServerConfig::builder()
+            .with_no_client_auth()
+            .with_single_cert(cert.clone(), private_key);
+        let config = Arc::new(server_config.unwrap());
+        //renaming it config back to server_config after Arc<.unwrap()>ing it;
+        let server_config = config;
 
-        let listener = std::net::TcpListener::bind(format!(":{}", 4443))
+        let listener = std::net::TcpListener::bind(format!("192.168.7.13:{}", 443))
             .unwrap();
         for stream in listener.incoming() {
                 let mut stream = stream.unwrap();
@@ -31,7 +41,7 @@ fn main() {
                                 break accepted;
                         };
                 };
-                match accepted.into_connection(server_config.clone()) {
+                match accepted.into_connection(server_config.clone()){
                         Ok(mut conn ) => {
                          let msg = concat!(
                             "HTTP/1.1 200 OK\r\n",
@@ -57,36 +67,40 @@ fn main() {
         }
 }
 
-struct TestPki {
-    server_cert_der: CertificateDer<'static>,
-    server_key_der: PrivateKeyDer<'static>,
+// struct TestPki {
+//     server_cert_der: CertificateDer<'static>,
+//     server_key_der: PrivateKeyDer<'static>,
+// }
+
+impl TestPki {
+          fn mew() -> Self {
+              let alg = &rcgen::PKCS_ECDSA_P256_SHA256;
+              let mut ca_params = rcgen::CertificateParams::new(Vec::new()).unwrap();
+              ca_params.subject_alt_names.push(rcgen::SanType::);
+             ca_params.distinguished_name.push(rcgen::DnType::OrganizationalUnitName,"Connie");
+             ca_params.distinguished_name.push(rcgen::DnType::CommonName, "connieserver");
+          }
 }
 
-// impl TestPki {
-//      fn mew() -> Self {
-//          let alg = &rcgen::PKCS_ECDSA_P256_SHA256;
-//          let mut ca_params = rcgen::CertificateParams::new(Vec::new()).unwrap();
-//          ca_params.subject_alt_names.push(rcgen::SanType::);
-// //         ca_params.distinguished_name.push(rcgen::DnType::OrganizationalUnitName,"Connie");
-// //         ca_params.distinguished_name.push(rcgen::DnType::CommonName, "connieserver");
-//      }
-//  }
+    fn load_cert(path: &str) -> Result<Vec<CertificateDer>, io::Error>  {//io::Result<Vec<CertificateDer>> {
+        let cert_file = File::open(path).unwrap();
+        let mut reader = BufReader::new(cert_file);
+        let certs : Vec<CertificateDer> = rustls_pemfile::certs(&mut reader);
 
-fn load_cert(path: &str) -> io::Result<Vec<CertificateDer>> {
-    let cert_file = File::open(path);
-    let mut reader = BufReader::new(cert_file);
-    let certs =  rustls_pemfile::certs(&mut reader).map_err(|_| io::Error::new(ErrorKind::InvalidData, "Invalid Certificates"))?;
-    Ok(certs)
-}
+        Err(Error::new(ErrorKind::InvalidData, "Invalid Certificate")).expect("TODO: panic message");
+        Ok(certs)
+    }
 
-fn load_private_certificate_key(path: &str) -> io::Result<PrivateKeyDer> {
-    let private_key_file = File::open(path);
-    let mut reader = BufReader::new(private_key_file);
-    let keys = rustls_pemfile::pkcs8_private_keys(&mut reader)
-        .or_else(|_| rustls_pemfile::rsa_private_keys(&mut reader))
-        .map_err(|_| io::Error::new(ErrorKind::InvalidData, "Invalid Private Keys"))?;
-    Ok(keys[0].clone())
-}
+    fn load_private_certificate_key(path: &str) -> io::Result<Vec<PrivateKeyDer>> {
+        let private_key_file = File::open(path)?;
+        let mut reader = BufReader::new(private_key_file);
+
+        let keys : Vec<PrivateKeyDer> = rustls_pemfile::pkcs8_private_keys(&mut reader);
+
+        Ok(keys)
+
+    }
+
 
 
 
