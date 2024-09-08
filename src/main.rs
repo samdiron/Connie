7edc15bed795c9f3a1351a10576e53d89cc7824d
samdiron <samer1787rizk@gmail.com>
2024-09-08 18:42:25 +0300

    update

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,6 @@
-//pass
+//TODO do shit here
+use sysinfo::System;
+
+fn main () {
+    println!("{}", System::host_name().unwrap());
+}
---
+++ src/startchecks/src/main.rs
@@ -0,0 +1,10 @@
+use sysinfo::{
+    System,
+    Components,
+    Cpu,
+    Networks,
+    DiskUsage
+};
+fn main() {
+    println!("Hello, world!");
+}
