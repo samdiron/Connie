8ab611fb0bf2d62252dfc5edad3f58b0aee1ef77
samdiron <samer1787rizk@gmail.com>
2024-09-24 03:26:06 +0300

    why

--- start/src/main.rs
+++ start/src/main.rs
@@ -1,3 +1,4 @@
+use std::string::String;
 use std::fs::File;
 use std::io::{stdin, stdout, Error, ErrorKind, Write};
 use std::mem::take;
@@ -44,30 +45,30 @@ fn firstTime() {
 
     println!("process: creating file /.config/connie/connie_config.yaml");
     println!("//NOTE cant be more than 17 char or less than 3 it cant contain spaces");
-    print!("name: ");
+    print!("server name: ");
     stdout().flush().unwrap();
     let mut server_name_string: String = String::new();
     stdin().read_line(&mut server_name_string);
 
     let server_name = server_name_string.trim_ascii_end();
-    if server_name.len() >= 16 {
+    if server_name.chars().count() >= 17 {
         loop {
            stdin().read_line(&mut server_name_string);
-            if server_name_string.trim_ascii_end().len() <= 16 {
+            if server_name_string.trim_ascii_end().chars().count() <= 17 {
                 break;
             }
-            else if server_name_string.trim_ascii_end().contains(" ") {
+            else if server_name_string.trim_ascii_end().chars().all(char::is_whitespace) {
                 print!("can't have spaces");
             } else {
-                println!("you are a dumb fuck; 16 or less");
+                println!("invalied");
             }
             //println!("how hard is it to enter a name that's more than 3 char less than 17;");
         }
     }
-    if server_name_string.trim_ascii_end().len() <= 2 {
+    if server_name_string.trim_ascii_end().chars().count() <= 3 {
         stdin().read_line(&mut server_name_string);
         loop {
-            if server_name_string.trim_ascii_end().len() >= 3 {
+            if server_name_string.trim_ascii_end().chars().count() >= 3 {
                 break
             } else {
                 println!("too short");
@@ -88,6 +89,7 @@ fn firstTime() {
             print!("enter a number: ");
             stdout().flush().unwrap();
             stdin().read_line(&mut server_name_string);
+            let is_max_client_number = max_client.chars().all(char::is_numeric);
             if is_max_client_number {
                 break;
             } else {
@@ -95,12 +97,9 @@ fn firstTime() {
             }
         }
     };
-    println!(
-        "\
-        server: 0 \
-        client & server: 1
-    ");
-    print!("choose a server status(0/1/2): ");
+    println!("server: 0 ");
+    println!("client & server: 1");
+    print!("choose a server status(0/1): ");
     stdout().flush().unwrap();
     let mut status_string : String = String::new();
     // stdin().read_line(&mut status_string);
@@ -124,9 +123,66 @@ fn firstTime() {
 
 
     println!("finshed getting server's identity");
-    println!("now getting the user's identity if you choose server status = 0 this will be the admin user");
+    println!("now getting the user's identity this will be the admin user for the server and a user to connect to other servers");
     //TODO input  = {password , name , username} data = {input, isadmin, id, uuid, + registration server uuid  }
 
+    let mut name = String::new();
+    let _ = loop {
+        println!("name should be 3~20 charcters of any language ");
+        print!("name: ");
+        stdout().flush().unwrap();
+        let mut name_string: String = String::new();
+        stdin().read_line(&mut name_string);
+
+        let name_str = name_string.trim_ascii_end().to_owned();
+        let is_valied = is_valied_str(&name_str);
+        if (name_str.chars().count() <= 20) && (name_str.chars().count() >= 3) && (is_valied == true) {
+            *&mut name = name_str;
+            break
+        }
+        else {
+            println!("enter a valid name ");
+        };
+    };
+
+
+    //TODO name before username
+    let mut user_name = String::new();
+    let _ = loop {
+        println!("username should be no spaces 3~20 charcters of any language numbers punctuation ");
+        print!("username: ");
+        stdout().flush().unwrap();
+        let mut user_name_string: String = String::new();
+        stdin().read_line(&mut user_name_string).unwrap();
+
+        let user_name_str = user_name_string.trim_ascii_end().to_owned();
+        let is_valied = is_valied_str(&user_name_str);
+        if (user_name_str.chars().count() <= 20) && (user_name_str.chars().count() >= 3) && (is_valied == true) {
+            *&mut user_name = user_name_str;
+            break
+        }
+        else {
+            println!("enter a valid username ");
+        };
+    };
+    let mut password = String::new();
+    let _ = loop {
+        println!("password can be 3~20 charcters and numbers punctuation ");
+        print!("password: ");
+        stdout().flush().unwrap();
+        let mut password_string: String = String::new();
+        stdin().read_line(&mut password_string);
+
+        let password_str = password_string.trim_ascii_end().to_owned();
+        let is_valied = is_valied_str(&password_str);
+        if (password_str.chars().count() <= 20) && (password_str.chars().count() >= 3) && (is_valied==true) {
+                *&mut user_name = password_str;
+                break
+        }
+        else {
+            println!("enter a valid name ");
+        };
+    };
 
 
     // if status.chars().count() > 1 {
@@ -168,4 +224,22 @@ fn firstTime() {
 
 
 }
-
+fn is_valied_str(s : &String) -> bool {
+    let numerics = s.chars().filter(|c| c.is_numeric()).count();
+    let letters = s.chars().filter(|c| c.is_alphabetic()).count();
+    let punc = s.chars().filter(|c| c.is_ascii_punctuation()).count();
+    let length = s.chars().count();
+    let total = numerics + letters + punc;
+    if total == length {
+        return true
+    }
+    else {return false};
+}
+// fn main() {
+//     let mut value = "asdfg";
+//
+//     let boolean = is_valied_str(value) ;
+//     if boolean == true {
+//         println!("yas");
+//     }else { println!("nah") }
+// }
