cc76462048b677334baa0c42e2429ffd1f3be7e9
samdiron <samer1787rizk@gmail.com>
2024-10-12 00:05:25 +0300

    path format

--- lib_start/src/dependencies/ld_nix.rs
+++ lib_start/src/dependencies/ld_nix.rs
@@ -13,7 +13,8 @@ fn check(home_path : &str) -> u8 {
     else {
         let error_data = format!("{},\n",(Error::new(ErrorKind::NotFound , "nix package manager not found"))).as_bytes();
         println!("{}",error_data);
-        let mut  file = File::open("{}/logs.csv",home_path).expect("could not open logs.csv");
+        let path = format!("{}/logs.csv",home_path);
+        let mut  file = File::open(path).expect("could not open logs.csv");
         file.write_all(error_data).expect("could not write to logs.csv");
         return 1
     }
