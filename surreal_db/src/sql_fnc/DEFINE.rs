bb905db97ba65ce956806fda5416056c8bcdf9ce
samdiron <samer1787rizk@gmail.com>
2024-11-26 13:20:06 +0200

    update
    fn user_scope ;
    fn first_time_db_def;
    compiled

--- surreal_db/src/sql_fnc/DEFINE.rs
+++ surreal_db/src/sql_fnc/DEFINE.rs
@@ -1,16 +1,45 @@
-// use//surrealdb::sql;
 use crate::db::DB;
-// use crate::user::sign_up::User;
+use surrealdb::Response;
 
-pub async fn define_scope_admin() {
+pub static USER_SESSION_TIME: u8 = 12;
+pub static ADMIN_SEEION_TIME: u8 = 24;
+
+pub async fn define_scope_admin() -> surrealdb::Result<Response> {
     let db = DB.clone();
-    let sql = "
-    DEFINE SCOPE IF NOT EXISTS  admin SESSION 24h
-	  SIGNUP ( CREATE admin SET name = $name, user_name = $user_name, pass = crypto::argon2::generate($pass), is_admin = $is_admib  )
-	  SIGNIN ( SELECT * FROM admin WHERE cpid = $cpid AND crypto::argon2::compare(pass, $pass) )"
-;
-    let _see = db.query(sql).await.expect("TODO : error msg");
+    let sql = format!("
+    DEFINE SCOPE IF NOT EXISTS  admin SESSION {ADMIN_SEEION_TIME}h
+	  SIGNUP ( CREATE admin SET name = $name, user_name = $user_name, pass = crypto::argon2::generate($pass), is_admin = $is_admin  )
+	  SIGNIN ( SELECT * FROM admin WHERE cpid = $cpid AND crypto::argon2::compare(pass, $pass) )");
+    let query_result = db.query(sql).await;
+    let err_is_ok = query_result.is_ok();
+    if err_is_ok {
+        let query_result = query_result.unwrap();
+        println!("query executed successfully");
+        Ok(query_result)
+    } else {
+        let err = query_result.unwrap_err();
+        Err(err)
+    }
 }
+
+pub async fn define_scope_user() -> surrealdb::Result<Response> {
+    let db = DB.clone();
+    let sql = format!("
+    DEFINE SCOPE IF NOT EXISTS  user SESSION {USER_SESSION_TIME}h
+	  SIGNUP ( CREATE admin SET name = $name, user_name = $user_name, pass = crypto::argon2::generate($pass), is_admin = $is_admib  )
+	  SIGNIN ( SELECT * FROM admin WHERE cpid = $cpid AND crypto::argon2::compare(pass, $pass) )");
+    let query_result = db.query(sql).await;
+    let err_is_ok = query_result.is_ok();
+    if err_is_ok {
+        let query_result = query_result.unwrap();
+        println!("query executed successfully");
+        Ok(query_result)
+    } else {
+        let err = query_result.unwrap_err();
+        Err(err)
+    }
+}
+
 // TODO: user scope function
 
 // pub async fn define_scope_user() {
