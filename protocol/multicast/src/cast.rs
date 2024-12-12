bf543e1077f5f96d3e543a6a50412d4fed48e325
samdiron <samer1787rizk@gmail.com>
2024-12-12 22:21:45 +0200

    update

--- protocol/multicast/src/cast.rs
+++ protocol/multicast/src/cast.rs
@@ -1,22 +1,26 @@
-use std::net::{IpAddr, SocketAddr};
+use std::{
+    net::UdpSocket,
+    net::{IpAddr, SocketAddr},
+};
 
 use common_lib::cheat_sheet::MULTICAST_PORT;
-use tokio::net::UdpSocket;
+// use tokio::net::UdpSocket;
 
 // TODO : make athis function
 // async fn validate_multicast() {
 // used to validate the multicast an then do a handshake and to look up
 // if the unit is a connie and if it's already connected before
 // example of handshake req {
-//      status: 0/1 \#/
-//      srerver_name or client_name \#/
-//      cpid \#/
-//      (ready to connect status: 0/1) \#/
+//      local/public :0/1 ?.
+//      status: 0/1 ?.
+//      srerver_name or client_name ?.
+//      cpid ?.
+//      (ready to connect status: 0/1) ?.
 //      (connie Version) .
 //
 //
 // }
-// note \#/ is used as a split psattern due to being not a common
+// note ?. is used as a split psattern due to being not a common
 // pattern for names or password
 // or cpid
 // and at the end a single . to mark the end of the handshake req
@@ -29,15 +33,14 @@ use tokio::net::UdpSocket;
 // to save power we could replace the ip from the msg by using recv_from src ;
 pub async fn cast_and_buffer(ip: IpAddr, command: u8) {
     let addr = SocketAddr::new(ip, MULTICAST_PORT);
-    let socket = UdpSocket::bind(addr).await.unwrap();
+    let socket = UdpSocket::bind(addr).unwrap();
     if command == 0 {
         let msg = b"hello world is any body there\n";
-        socket.send(msg).await.unwrap();
+        socket.send(msg).unwrap();
         // TODO make the buffer smaller
         let mut buffer1 = Vec::new();
         let (size, src) = socket
             .recv_from(&mut buffer1)
-            .await
             .expect("could not get buffer");
         println!("source: {}", src);
         let str_answer = String::from_utf8_lossy(&buffer1[..size]);
@@ -45,8 +48,8 @@ pub async fn cast_and_buffer(ip: IpAddr, command: u8) {
     }
     if command == 1 {
         loop {
-            let mut buffer = Vec::new();
-            let (size, src) = socket.recv_from(&mut buffer).await.unwrap();
+            let mut buffer = vec![0; 200];
+            let (size, src) = socket.recv_from(&mut buffer).unwrap();
             if size > 0 {
                 let string_msg = String::from_utf8_lossy(&buffer[..size]);
                 let string_msg = string_msg.into_owned();
---
+++ protocol/tcp/src/session/client/connector.rs
@@ -0,0 +1,30 @@
+// use std::io::{Read, Stdout, Write};
+use std::io::Write;
+use std::net::{IpAddr, SocketAddr, TcpStream};
+use std::str::FromStr;
+use std::sync::Arc;
+
+pub fn tcp_connector() {
+    let root_store =
+        rustls::RootCertStore::from_iter(webpki_roots::TLS_SERVER_ROOTS.iter().cloned());
+    let config = rustls::ClientConfig::builder()
+        .with_root_certificates(root_store)
+        .with_no_client_auth();
+    let ip = IpAddr::from_str("ip_from the multicast search or a known server").unwrap();
+    let port = common_lib::cheat_sheet::TCP_MAIN_PORT;
+    let socketaddr = SocketAddr::new(ip, port);
+    let server_name = "connie".try_into().unwrap();
+    let mut conn = rustls::ClientConnection::new(Arc::new(config), server_name).unwrap();
+    let mut sock = TcpStream::connect(socketaddr).unwrap();
+    let mut tls = rustls::Stream::new(&mut conn, &mut sock);
+    let text = b"text from a client ?.";
+    let _res = tls.write_all(text).unwrap();
+    let ciphersuite = tls.conn.negotiated_cipher_suite().unwrap();
+
+    writeln!(
+        &mut std::io::stderr(),
+        "current suite: {:?}",
+        ciphersuite.suite()
+    )
+    .unwrap();
+}
