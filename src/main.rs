bf3f871a641102328818eb07ddee97445464e29b
samdiron <samer1787rizk@gmail.com>
2024-12-13 23:40:18 +0200

    update to a loop

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
-#![allow(clippy::clone_on_copy)]
+use std::process::exit;
+
 // use std::process::exit;
-use clap::{command, ArgGroup, Arg, ArgMatches};
+use clap::{command, Arg, ArgGroup, ArgMatches};
 use lib_start::init::start;
 use tokio::runtime::Runtime;
-//args start connect bind verbose sql max-clients only-users web-host  
+//args start connect bind verbose sql max-clients only-users web-host
 fn main() {
     let match_result: ArgMatches = command!()
         .about("Connie is a private home server used to connect with other connie servers privately it can stream movies music and store private files ")
@@ -13,6 +14,11 @@ fn main() {
             .long("start")
             .num_args(0)
             .help("ready to connect/bind mode"))
+        .arg(Arg::new("quite")
+            .short('q')
+            .long("quite")
+            .num_args(0)
+            .help("exits the program"))
         .group(ArgGroup::new("server-user"))
         .arg(Arg::new("bind")
             .short('b')
@@ -31,39 +37,38 @@ fn main() {
         //     .long("connect"))
         // .arg(Arg::new("verbose").short("v").long("verbose"))    
         .get_matches();
-    
-    // make a PID lock file in start
-     let rt = Runtime::new().unwrap();
 
-    let is_ready : bool =  match_result.get_one::<bool>("start").unwrap().clone();
+    // make a PID lock file in start
+    let rt = Runtime::new().unwrap();
+
+    let is_ready: bool = match_result.get_one::<bool>("start").unwrap().clone();
 
     println!("arg passed");
     if is_ready {
-
         let machine = rt.block_on(start()).expect("could not get machine info");
-        println!("{}",machine.host_name);
-        println!("connie is up and ready");
-        let matches = rt.block_on(commands()).expect("could not get user input").clone();
-        let matches_ = matches.get_one::<bool>("connect").unwrap().clone();
-        if matches_ {
-        println!("okay");
+        println!("she is onnn");
+        println!("{} , says hello", machine.host_name.as_str());
+        loop {
+            let quite = match_result.get_one::<bool>("quite").unwrap().clone();
+            if quite {
+                exit(0)
+            }
         }
     }
 }
-
-
-async fn commands() -> std::io::Result<ArgMatches> {
-let match_result: ArgMatches = command!()
-        .about("Connie is a private home server used to connect with other connie servers privately it can stream movies music and store private files ")
-        .group(ArgGroup::new("server-user"))
-       .arg(Arg::new("bind")
-            .short('b')
-            .long("bind")
-            .conflicts_with("connect")
-            .help("binds connie to local_ip:4443 tcp"))
-        .arg(Arg::new("connect")
-            .long("connect"))
-        .arg(Arg::new("verbose").long("verbose"))    
-        .get_matches();
-    Ok(match_result)
-} 
+//
+// async fn commands() -> std::io::Result<ArgMatches> {
+//     let match_result: ArgMatches = command!()
+//         .about("Connie is a private home server used to connect with other connie servers privately it can stream movies music and store private files ")
+//         .group(ArgGroup::new("server-user"))
+//        .arg(Arg::new("bind")
+//             .short('b')
+//             .long("bind")
+//             .conflicts_with("connect")
+//             .help("binds connie to local_ip:4443 tcp"))
+//         .arg(Arg::new("connect")
+//             .long("connect"))
+//         .arg(Arg::new("verbose").long("verbose"))
+//         .get_matches();
+//     Ok(match_result)
+// }
