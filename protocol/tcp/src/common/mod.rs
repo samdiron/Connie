cf934638aa5852137cec6408825442a20c938eac
samdiron <samer1787rizk@gmail.com>
2025-01-05 02:07:05 +0200

    request file

--- protocol/tcp/src/common/mod.rs
+++ protocol/tcp/src/common/mod.rs
@@ -1,2 +1,3 @@
 pub mod chunk;
 pub mod handshake;
+pub mod request;
---
+++ protocol/tcp/src/common/request.rs
@@ -0,0 +1,12 @@
+// AUTH
+pub const JWT_AUTH: &str = "0";
+pub const LOGIN_CRED: &str = "1";
+pub const SIGNIN_CRED: &str = "2";
+
+//request Head
+pub const GET: &str = "!G";
+pub const POST: &str = "!P";
+pub const DELETE: &str = "!D";
+
+pub const SPLIT: &str = "\n";
+pub const END: &str = "\0";
