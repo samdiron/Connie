b7e3fca41b7be250c7f516c3247d8a38b721fd7c
samdiron <smaer1787rizk@gmail.com>
2025-01-23 09:22:23 +0000

    update
    using green threads instead of one to one threads

--- tcp/src/server/listener.rs
+++ tcp/src/server/listener.rs
@@ -1,17 +1,31 @@
-use std::thread::available_parallelism;
+use std::io::Read;
 use std::net::SocketAddr;
 use common_lib::cheat_sheet::{TCP_MAIN_PORT,LOCAL_IP};
 use lib_db::types::PgPool;
 use log::warn;
-use tokio::net::TcpListener;
-use super::pool::ThreadPool;
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::{net::TcpListener, task};
+use tokio::net::TcpStream;
 
+
+async fn handle(st: (TcpStream, SocketAddr), pool: PgPool) -> std::io::Result<()> {
+    let _p = pool;
+    let mut buf = String::new();
+    let mut stream = st.0;
+    let addr = st.1;
+    println!("client: {addr} is being served");
+    stream.read_to_string(&mut buf).await?;
+    let mut f = std::fs::File::open("../../../res.html")?;
+    let mut buff = vec![0; 4000];
+    let file_size = f.read_to_end(&mut buff)?;
+    stream.write_all(&buff).await?;
+    println!("file_size: {file_size}");
+    Ok(())
+
+}
 
 
 pub async fn bind(pool: PgPool) {
-    let size = usize::from(available_parallelism().unwrap());
-    let threads = ThreadPool::new(size, pool);
-
     let ip = LOCAL_IP.clone();
     let addr = SocketAddr::new(ip, TCP_MAIN_PORT.clone());
     let socket = TcpListener::bind(addr).await.unwrap();
@@ -19,7 +33,18 @@ pub async fn bind(pool: PgPool) {
         match socket.accept().await {
             Ok(stream) => {
                 println!("stream ");
-                threads.execute(stream);
+                let inner_p = pool.clone();
+                match task::spawn(async{
+                            match handle(stream, inner_p).await {
+                                Ok(..) => {},
+                                Err(..) => {},
+                            }
+                            }).await {
+                    Ok(_) => {
+                        println!("a client was server");
+                    },
+                    Err(_) => {},
+                };
                 
             }
             Err(e) => {
