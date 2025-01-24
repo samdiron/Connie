abc3c82f95003f27ceff03c66545fec54b9c5b55
samdiron <smaer1787rizk@gmail.com>
2025-01-24 21:06:38 +0000

    "cher is life"

---
+++ tcp/src/client/connector.rs
@@ -0,0 +1,12 @@
+use std::net::{IpAddr, SocketAddr};
+use std::io;
+use common_lib::cheat_sheet::TCP_MAIN_PORT;
+use tokio::net::TcpStream;
+
+pub async fn connect_tcp(ip: IpAddr) -> io::Result<()> {
+    let port = TCP_MAIN_PORT;
+    let addr = SocketAddr::new(ip, port);
+    let mut stream = TcpStream::connect(addr).await?;
+
+    Ok(())
+}
