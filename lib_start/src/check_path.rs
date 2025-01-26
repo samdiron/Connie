9be25ed6feeb6530f3891c8de089b4f4e08b7bc9
samdiron <samer1787rizk@gmail.com>
2025-01-26 17:23:01 +0000

    update

---
+++ lib_start/src/check_path.rs
@@ -0,0 +1,36 @@
+use std::{fs::{exists, File}, io::Result};
+
+fn check_first_time() -> Result<u8> {
+    let state: u8;
+    let path: Vec<&str> = vec![
+        "/Connie/etc/db_conn.yaml",
+        "/Connie/etc/cie_ident.yaml",
+        "/Connie/etc/cie_config.yaml",
+
+    ];
+    let mut tmp: Vec<bool> = Vec::with_capacity(4);
+    for p in path {
+        let exist = exists(p)?;
+        if exist {
+            let f = File::open(p)?;
+            let data = f.metadata()?;
+            let len = data.len();
+            if len > 0 {
+                tmp.push(true);
+            }
+            else {
+                tmp.push(false);
+            }
+        }
+    }
+    if tmp.contains(&false) {
+        state = 0;
+    }else {
+        state = 1;
+    }
+
+
+
+
+    Ok(state)
+}
--- lib_start/src/lib.rs
+++ lib_start/src/lib.rs
@@ -1,14 +1,5 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+pub mod init;
+pub(crate) mod get_config;
+pub(crate) mod check_path;
+pub(crate) mod checks;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
