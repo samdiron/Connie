0357ddce1cefc5424ed9b31eb0a665308c734790
samdiron <samer1787rizk@gmail.com>
2025-03-09 20:15:28 +0000

    update

--- tcp/src/common/mod.rs
+++ tcp/src/common/mod.rs
@@ -3,6 +3,29 @@
 #[allow(dead_code)]
 pub(crate) mod request;
 
+
+
+
+
+
+pub(crate) mod tls {
+    use std::io::{Result, Write};
+
+    use crate::client::client::Client;
+    pub enum Tls {
+        Client
+    }
+    
+
+}
+
+
+
+
+
+
+
+
 #[allow(dead_code)]
 pub(crate) mod handshakes {
     use std::io;
