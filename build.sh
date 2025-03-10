dfac4dace0962885779d59232255d7d7888000bc
samdiron <samer1787rizk@gmail.com>
2025-03-10 23:04:05 +0000

    update

--- build.sh
+++ build.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
 
-read -p "what to build (All/debug/client/release):(a/d/c/r): " type
+read -p "what to build (All/debug/client/release/windows):(a/d/c/r/w): " type
 if [ $type == "a" ] ; then
   echo "building all now"
 
   echo "building release"
   cargo build  --release
   echo "done"
-
+  
+  echo "windows release "
+  cargo build --target=x86_64-pc-windows-gnu --release
+  echo "done"
+  
   echo "building client"
   cargo build  --profile client
   echo "done"
@@ -18,24 +22,30 @@ if [ $type == "a" ] ; then
   echo "done"
   clear
   echo "size"
-  ls -lh ./target/debug/Connie
-  ls -lh ./target/client/Connie
-  ls -lh ./target/release/Connie
+  ls -lh ./target/debug/cie
+  ls -lh ./target/client/cie
+  ls -lh ./target/release/cie
+  ls -lh ./target/x86_64-pc-windows-gnu/release/cie.exe
 elif [ $type == "r" ] ; then 
   echo "building release"
   cargo build -q --release
   clear
-  ls -lh ./target/release/Connie
+  ls -lh ./target/release/cie
 elif [ $type == "c" ] ; then 
   echo "building client"
   cargo build -q --profile client
   clear
-  ls -lh ./target/client/Connie
+  ls -lh ./target/client/cie
 elif [ $type == "d" ] ; then 
   echo "building debug now"
-  cargo builad -q
+  cargo build -q
   clear
-  ls -lh ./target/debug/Connie
+  ls -lh ./target/debug/cie
+elif [ $type == "w" ] ; then 
+  echo "building windows now"
+  cargo build --target=x86_64-pc-windows-gnu --release
+  clear
+  ls -lh ./target/x86_64-pc-windows-gnu/release/cie.exe
 else 
   echo "please enter a valid argument. "
   exit 1
