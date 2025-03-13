fb6c6d9e1959ee0a571b79c8da2c6f11fd70b9df
samdiron <samer1787rizk@gmail.com>
2025-03-13 11:46:04 +0000

    escaped all user input

--- lib_db/src/fncs.rs
+++ lib_db/src/fncs.rs
@@ -2,6 +2,15 @@ use std::str::FromStr;
 use uuid::Error;
 use common_lib::rand::{self, distributions::Alphanumeric, Rng}; 
 
+
+
+
+
+pub fn escape_user_input(s: &String) -> String{
+    let bind = s.replace("'", "");
+    bind
+}
+
 pub fn random_string(chars: u8) -> String{
     let s: String = rand::thread_rng()
         .sample_iter(&Alphanumeric)
--- lib_db/src/user/user_struct.rs
+++ lib_db/src/user/user_struct.rs
@@ -3,6 +3,7 @@ use common_lib::log::debug;
 use sha256::digest;
 use sqlx::{PgPool, Result, Row};
 use uuid::Uuid;
+use crate::fncs::escape_user_input;
 
 pub struct User {
     pub cpid: String,
@@ -18,11 +19,13 @@ pub async fn validate_claim_wcpid(
     pool: &PgPool
 ) -> sqlx::Result<bool> {
     let table = r#" "user" "#;
+    let clean_paswd = escape_user_input(&paswd);
+    let cleen_cpid = escape_user_input(&cpid);
     let sql = format!(
         "SELECT count(1) FROM {} WHERE cpid = '{}' AND password = '{}' ;",
         table,
-        cpid,
-        paswd
+        cleen_cpid,
+        clean_paswd
     );
     let _count = sqlx::query(&sql).fetch_one(pool).await?;
     let count: i64 = _count.get("count");
@@ -39,11 +42,13 @@ pub async fn validate_claim(
     pool: &PgPool
 ) -> sqlx::Result<bool> {
     let table = r#" "user" "#;
+    let clean_paswd = escape_user_input(&paswd);
+    let cleen_name = escape_user_input(&name);
     let sql = format!(
         "SELECT count(1) FROM {} WHERE name = '{}' AND password = '{}' ;",
         table,
-        name,
-        paswd,
+        cleen_name,
+        clean_paswd,
     );
     let _count = sqlx::query(&sql).fetch_one(pool).await;
     if _count.is_err() {Ok(false)}else {
@@ -61,6 +66,8 @@ pub async fn fetch_wcpid(
     _password: String,
     pool: &PgPool,
 ) -> sqlx::Result<User, sqlx::Error> {
+    let cpid = escape_user_input(&cpid);
+    let _password = escape_user_input(&_password);
     let sql = format!(r#"SELECT * FROM "user" WHERE cpid = '{}' AND password = '{}';"#, cpid , _password);
     debug!("SERVER_DB: {}",&sql);
     let row = sqlx::query(&sql)
@@ -84,11 +91,15 @@ pub async fn fetch(
     _password: String,
     pool: &PgPool,
 ) -> sqlx::Result<User, sqlx::Error> {
-    let sql = r#"SELECT * FROM "user" WHERE name = $1 AND password = $2;"#;
+    let table = r#" "user" "#;
+    let name = escape_user_input(&name);
     let password = sha256::digest(_password);
-    let row = sqlx::query(sql)
-        .bind(name)
-        .bind(password)
+    let sql = format!(
+        "SELECT * FROM {table} WHERE name = '{}' AND password = '{}';",
+        name,
+        password
+    );
+    let row = sqlx::query(&sql)
         .fetch_one(pool)
         .await?;
     let user = User {
@@ -104,28 +115,33 @@ pub async fn fetch(
 
 impl User {
     pub async fn create(self, pool: &PgPool) -> Result<User, sqlx::Error> {
-        let sql = concat!(
+        let name = escape_user_input(&self.name);
+        let username = escape_user_input(&self.username);
+        let host = escape_user_input(&self.host);
+        let email = escape_user_input(&self.email);
+
+        let pass = digest(self.password);
+        let cpid = Uuid::new_v4().to_string();
+        let sql = format!(
             r#"INSERT INTO "user"
             (cpid, name, username, host, email, password)
-            VALUES ($1, $2, $3, $4, $5, $6);"#
+            VALUES ('{}', '{}', '{}', '{}', '{}', '{}');"#,
+            &cpid,
+            &name,
+            &username,
+            &host,
+            &email,
+            &pass
         );
-        let cpid = Uuid::new_v4().to_string();
-        let pass = digest(self.password);
-        let _res = sqlx::query(sql)
-            .bind(cpid.clone())
-            .bind(&self.name)
-            .bind(&self.username)
-            .bind(&self.host)
-            .bind(&self.email)
-            .bind(pass.clone())
+        let _res = sqlx::query(&sql)
             .execute(pool)
             .await?;
         let new_user = User {
             cpid,
-            name: self.name,
-            username: self.username,
-            host: self.host,
-            email: self.email,
+            name,
+            username,
+            host,
+            email,
             password: pass,
         };
         Ok(new_user)
