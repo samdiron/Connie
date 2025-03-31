f34060d522f48869313265a03123f9296f61235f
samdiron <samer1787rizk@gmail.com>
2025-03-31 17:43:12 +0000

    starting the tui

--- tui/src/lib.rs
+++ tui/src/lib.rs
@@ -1,14 +1,14 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use std::io::Stdout;
+
+use ratatui::{self, prelude::CrosstermBackend, Terminal};
+
+
+
+pub fn init() -> Terminal<CrosstermBackend<Stdout>> {
+    let terminal = ratatui::init();
+    terminal
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+pub fn restore_terminal() {
+    ratatui::restore();
 }
