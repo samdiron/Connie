51409333a6402530386a3ea46a0f3462bc50f43b
samdiron <samer1787rizk@gmail.com>
2025-06-27 19:55:10 +0300

    localizing packages

---
+++ extern/tokio-rustls/examples/client.rs
@@ -0,0 +1,80 @@
+use std::error::Error as StdError;
+use std::io;
+use std::net::ToSocketAddrs;
+use std::path::PathBuf;
+use std::sync::Arc;
+
+use argh::FromArgs;
+use rustls::pki_types::pem::PemObject;
+use rustls::pki_types::{CertificateDer, ServerName};
+use tokio::io::{copy, split, stdin as tokio_stdin, stdout as tokio_stdout, AsyncWriteExt};
+use tokio::net::TcpStream;
+use tokio_rustls::{rustls, TlsConnector};
+
+/// Tokio Rustls client example
+#[derive(FromArgs)]
+struct Options {
+    /// host
+    #[argh(positional)]
+    host: String,
+
+    /// port
+    #[argh(option, short = 'p', default = "443")]
+    port: u16,
+
+    /// domain
+    #[argh(option, short = 'd')]
+    domain: Option<String>,
+
+    /// cafile
+    #[argh(option, short = 'c')]
+    cafile: Option<PathBuf>,
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn StdError + Send + Sync + 'static>> {
+    let options: Options = argh::from_env();
+
+    let addr = (options.host.as_str(), options.port)
+        .to_socket_addrs()?
+        .next()
+        .ok_or_else(|| io::Error::from(io::ErrorKind::NotFound))?;
+    let domain = options.domain.unwrap_or(options.host);
+    let content = format!("GET / HTTP/1.0\r\nHost: {}\r\n\r\n", domain);
+
+    let mut root_cert_store = rustls::RootCertStore::empty();
+    if let Some(cafile) = &options.cafile {
+        for cert in CertificateDer::pem_file_iter(cafile)? {
+            root_cert_store.add(cert?)?;
+        }
+    } else {
+        root_cert_store.extend(webpki_roots::TLS_SERVER_ROOTS.iter().cloned());
+    }
+
+    let config = rustls::ClientConfig::builder()
+        .with_root_certificates(root_cert_store)
+        .with_no_client_auth(); // i guess this was previously the default?
+    let connector = TlsConnector::from(Arc::new(config));
+
+    let stream = TcpStream::connect(&addr).await?;
+
+    let (mut stdin, mut stdout) = (tokio_stdin(), tokio_stdout());
+
+    let domain = ServerName::try_from(domain.as_str())?.to_owned();
+    let mut stream = connector.connect(domain, stream).await?;
+    stream.write_all(content.as_bytes()).await?;
+
+    let (mut reader, mut writer) = split(stream);
+
+    tokio::select! {
+        ret = copy(&mut reader, &mut stdout) => {
+            ret?;
+        },
+        ret = copy(&mut stdin, &mut writer) => {
+            ret?;
+            writer.shutdown().await?
+        }
+    }
+
+    Ok(())
+}
