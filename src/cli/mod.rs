f1aa3720217e7f908cbd19da9d181ff3c5f10dfb
samdiron <samer1787rizk@gmail.com>
2025-09-22 13:51:22 +0300

    fixup: removing a module that does not exist on the remote branch

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -2,7 +2,7 @@ pub(crate) mod cli_db;
 pub(crate) mod cli_dev;
 pub(crate) mod cli_bind;
 pub(crate) mod cli_user;
-pub(crate) mod cli_admin;
+// pub(crate) mod cli_admin;
 pub(crate) mod cli_server;
 pub(crate) mod cli_request;
 
